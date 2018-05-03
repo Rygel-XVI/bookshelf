facb489d38a6ce1ebcb6c3ff481d825fb08b165e
Rygel-XVI <micha232@umn.edu>
2018-05-03 01:02:40 -0700

    debugging callback

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,6 +5,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def tumblr
+    byebug
     @user = User.from_omniauth(request.env["omniauth.auth"])
     byebug
     sign_in_and_redirect @user
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-   devise :omniauthable, :omniauth_providers => [:google_oauth2, :tumblr, :twitter, :github]
+  devise :omniauthable, :omniauth_providers => [:google_oauth2, :tumblr, :twitter, :github]
+
+
+# auth.uid = rygel-xvi  teach-with-your-hands
+# auth.info.avatar = profile pic
+# auth.info.nickname = nickname
+# auth.info.name = name
+
 
    def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
+      # user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
+    byebug
   end
 
 end
