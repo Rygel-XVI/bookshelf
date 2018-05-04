b5b7834b634cd32906f5968050fb7f18dd11b769
Rygel-XVI <micha232@umn.edu>
2018-05-04 02:24:52 -0700

    setup welcome controller and home page. user login works with omniauth and without

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,9 +12,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def google_oauth2
-    byebug
     @user = User.from_omniauth(request.env["omniauth.auth"])
-
+    sign_in_and_redirect @user
   end
 
   def github
