bebb067836d8dcb0eda436ef42f079a07ce76a8d
Rygel-XVI <micha232@umn.edu>
2018-06-13 20:11:03 -0700

    added user creation from users#new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,23 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    if User.find_by(name: params[:user][:name])
+      ##flash error message user already exists
+      redirect_to new_user_path
+    else
+      @user = User.create(user_params(params[:user]))
+      redirect_to user_path(@user)
+    end
   end
 
   def destroy
 
+  end
+
+  private
+
+  def user_params(args)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 
 end
