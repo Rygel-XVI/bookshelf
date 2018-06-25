4e0206bcd4252ee415ccc48555e672e240c362e4
Rygel-XVI <micha232@umn.edu>
2018-06-24 20:32:53 -0700

    added book update functionality

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -21,6 +21,12 @@ class Admin::BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end
+
   private
 
   def book_params
