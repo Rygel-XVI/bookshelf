ee380de0a1fa4741eb375222c95d32f3218f8d2c
Rygel-XVI <micha232@umn.edu>
2018-07-03 16:16:48 -0700

    added code from word doc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -56,4 +56,28 @@ class ApplicationController < ActionController::Base
     @author = Author.find(params[:id])
   end
 
+  # Stuff for checkout/return button
+
+  def can_return?
+    @Book.status == "Checked Out" && @user_books.status == "Checked Out"
+  end
+
+  def set_submit_message
+    can_return? ? "Check Out" : "Return Book"
+  end
+
+  def change_book_status
+    @book.status == "Available" ? "Checked Out" : "Available"
+  end
+
+  def user_book_status_options?
+    @book.status == "Available" ?  false  : true
+  end
+
+  def set_button_form_locals
+    @button = set_submit_message
+    @global_status = change_book_status
+    @user_status_choice = user_book_status_options
+  end
+
 end
--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -6,10 +6,14 @@ module AuthorsHelper
   #   authors.select {|a| a.books.empty}
   # end
 
+  # Returns the authors name
+  # Need to input variable because used in partials
   def author_name(author)
     author.name
   end
 
+  # Returns true or false if the author has any books associated with it
+  # Need to input variable because used in partials
   def no_books?(author)
     author.books.empty?
   end
