7a2d78955a48e01bf26cf8f35ea037b14d9f4a29
Rygel-XVI <micha232@umn.edu>
2018-07-05 20:58:57 -0700

    added logic to only delete a book if it has no associations else change status to graveyard. also flash msgs.

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -29,11 +29,22 @@ class Admin::BooksController < ApplicationController
     end
   end
 
+# Change method so it checks if it has any UserBooks and if those have actual Users associated before deleting
+
+####### TEST THIS OUT MORE
+
   def destroy
     set_book
-    binding.pry
-    @book.destroy
-    flash[:msg] = "#{@book.title} has been deleted."
+    if !@book.user_books.empty?
+      @userbooks = @book.user_books
+      if @userbooks.detect {|ub| User.find(ub.user_id)}
+        @book.update(status: "Graveyard")
+        flash[:msg] = "Cannot delete go talk to Annette if you need to delete it. Status changed to Graveyard instead."
+      end
+    else
+      @book.destroy
+      flash[:msg] = "#{@book.title} has been deleted."
+    end
     redirect_to books_path
   end
 
