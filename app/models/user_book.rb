215c5aa9cd8726d19849c447627061323d03045f
Rygel-XVI <micha232@umn.edu>
2018-07-06 18:35:13 -0700

    deleted unused code

--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -5,7 +5,4 @@ class UserBook < ApplicationRecord
   validates :status, presence: true
   validates_inclusion_of :status, in: ["Checked Out", "Read", "Not Read", "Returned"]
 
-  # def has_valid_user?
-  #   @userbooks.detect {|ub| User.find(ub.user_id)}
-  # end
 end
