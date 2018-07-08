9698cbc414dffbf7762a8527a0a22fa099b9dd5f
Rygel-XVI <micha232@umn.edu>
2018-07-08 10:52:23 -0700

    reorganized methods

--- app/models/book.rb
+++ app/models/book.rb
@@ -37,16 +37,6 @@ class Book < ApplicationRecord
     book_array.order(title: :asc)
   end
 
-  def self.avg_words(books)
-    words = 0
-    if books.size > 0
-      books.map {|book| words += book.word_count}
-      words =  words/books.size
-    else
-      words
-    end
-  end
-
   # In the future can implement multiple filters here
   def self.set_books_scope_to_status(status)
     case status
@@ -59,6 +49,18 @@ class Book < ApplicationRecord
       else
         Book.all
       end
+  end
+
+############## User Statistics
+
+  def self.avg_words(books)
+    words = 0
+    if books.size > 0
+      books.map {|book| words += book.word_count}
+      words =  words/books.size
+    else
+      words
+    end
   end
 
 ###### Determining if it can be moved between library and user
